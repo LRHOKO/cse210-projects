092d09985b6b71bfafb5d2f65ef27cd2a5ea56bc
LRHOKO <hokopoko1@hotmail.com>
2023-09-24 21:56:47 -0500

    Wrote grade checking program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+       
+        string letter = "";
+
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        } 
+         else if (gradeInt >= 80)
+        {
+            letter = "B";
+        } 
+         else if (gradeInt >= 70)
+        {
+            letter = "C";
+        } 
+          else if (gradeInt >= 60)
+        {
+            letter = "D";
+        } 
+        else 
+        {
+            letter = "F";
+        } 
+
+        int lastDigit = Math.Abs(gradeInt) % 10;
+        string  gradeModifiers = "";
+
+        if (lastDigit >= 7 && letter != "A" && letter != "F")
+        {
+            gradeModifiers = "+";
+        }
+
+        else if (lastDigit < 3 && letter != "F")
+        {
+            gradeModifiers = "-";
+        
+        }
+
+        Console.WriteLine($"Your grade is {letter}{gradeModifiers}");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulations!");
+        }
+        
+        else 
+        {
+            Console.WriteLine("You'll do better next time.");
+        }
     }
 }
