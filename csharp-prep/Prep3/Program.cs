479d8e8e363cb3912972ae63fb4f1bd1a17b862a
LRHOKO <hokopoko1@hotmail.com>
2023-09-24 23:14:45 -0500

    Wrote a random number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics.Tracing;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
+            {
+                guessCount++;
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! Your total number of guesses is {guessCount}");
+                }
+            }
+            Console.Write("Do you want to continue? (yes/no) ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "yes");
     }
 }
+
+
+
+
